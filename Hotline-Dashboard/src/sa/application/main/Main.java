10f803b0e7a428593b2e8c3f0baee35a12da4d1f
home <home@hp>
2018-09-30 16:36:07 +0200

    after adding the Date and time

--- Hotline-Dashboard/src/sa/application/main/Main.java
+++ Hotline-Dashboard/src/sa/application/main/Main.java
@@ -1,11 +1,21 @@
 package sa.application.main;
 
+import java.util.Scanner;
+
+import sa.application.finance.Income;
+
 public class Main {
-	
-	public static void main(String [] args) {
-		
-		System.out.println("Hello Jenny!");
-		
+
+	static Scanner scan = new Scanner(System.in);
+	public static Income inc;
+
+	public static void main(String[] args) {
+
+		System.out.println("Please enter your income this month: ");
+		inc = new Income(scan.nextDouble());
+		System.out.println("Income: " + inc.amount);
+		System.out.println("Monthly Account: " + inc.calc_MonthlyAccount(inc.amount));
+		System.out.println("");
 	}
 
 }
