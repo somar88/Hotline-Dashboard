b01e6ccf24a73b488e7aa239e1cfd5d518b4c624
home <home@hp>
2018-09-30 16:36:26 +0200

    After adding the date and time

---
+++ Hotline-Dashboard/src/sa/application/finance/Income.java
@@ -0,0 +1,39 @@
+package sa.application.finance;
+
+import java.time.LocalDateTime;
+
+public class Income {
+
+	public double amount = 0.0;
+	public String check_in;
+	public LocalDateTime timing = LocalDateTime.now();
+	
+	public Income(double amount) {
+		this.amount = amount;
+		
+	}
+
+	public void calculate() {
+
+	}
+
+	public double calc_MonthlyAccount(double amount) {
+		return amount - calc_BizExp(amount);
+	}
+
+	public double calc_BizExp(double amount) {
+		return amount * 0.10;
+	}
+
+	public double calc_HomeExpances(double amount) {
+
+		amount -= 410;
+		System.out.println("amount");
+
+		return 0;
+	}
+
+	public double calc_(double amount) {
+		return amount * 0.10;
+	}
+}
