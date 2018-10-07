8a9e6f0fe68f6a953730268935c867351a642343
somar <somar@Extreme-Laptop>
2018-10-07 21:45:14 +0200

    REMOVING#

--- Hotline-Dashboard/src/sa/application/main/Main.java
+++ Hotline-Dashboard/src/sa/application/main/Main.java
@@ -1,23 +1,12 @@
 package sa.application.main;
 
-import java.awt.Dimension;
 import java.util.Scanner;
-
-import javax.swing.JFrame;
 
 public class Main {
 
 	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
-		JFrame frm = new JFrame("Welcome");
-		frm.setSize(new Dimension(600, 400));
-		frm.setLocationRelativeTo(null);
-		frm.setResizable(false);
-		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frm.setVisible(true);	
-
 	}
 
 }
